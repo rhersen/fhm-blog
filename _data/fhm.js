725acd11da81f53908a6c116ee52fa11d2eff279
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-03-05 11:29:06 +0100

    abandoning this repo

---
+++ _data/fhm.js
@@ -0,0 +1,72 @@
+const book={}
+
+var parse = require('date-fns/parse')
+var isValid = require('date-fns/isValid')
+var format = require('date-fns/format')
+
+function iso(date) {
+  const parsed = parse(date, "M/d/yy", new Date());
+  return isValid(parsed) ? format(parsed, "yyyy-MM-dd") : date;
+}
+
+function cases({Sheets}) {
+  const sheet = Sheets?.["Antal per dag region"];
+  if (!sheet) return {};
+
+  const entries = Object.entries(sheet);
+
+  const columnKeys = Object.fromEntries(
+    entries
+      .filter(([cell]) => /^[^A]1$/.test(cell))
+      .map(([cell, value]) => [/^(\D+)(\d+)$/.exec(cell)[1], value.v])
+  );
+
+  const rowKeys = Object.fromEntries(
+    entries
+      .filter(([cell]) => /^A\d+$/.test(cell))
+      .filter(([cell]) => cell !== "A1")
+      .map(([cell, value]) => [/^(\D+)(\d+)$/.exec(cell)[2], value.w])
+  );
+
+  const columns = Object.fromEntries(
+    Object.entries(columnKeys).map(([, value]) => [value, {}])
+  );
+
+  entries
+    .filter(([cell]) => !/^A/.test(cell))
+    .filter(([cell]) => !/\D1$/.test(cell))
+    .forEach(([cell, value]) => {
+      const match = /^(\D+)(\d+)$/.exec(cell);
+      if (match)
+        columns[columnKeys[match[1]]][iso(rowKeys[match[2]])] = value.v;
+    });
+  return columns;
+}
+
+module.exports = () => ({
+  cases: cases(book),
+  population: {
+    Totalt_antal_fall: 10230000,
+    Blekinge: 159748,
+    Dalarna: 287795,
+    Gotland: 59636,
+    Gävleborg: 287333,
+    Halland: 333202,
+    Jämtland_Härjedalen: 130697,
+    Jönköping: 363351,
+    Kalmar: 245415,
+    Kronoberg: 201290,
+    Norrbotten: 250230,
+    Skåne: 1376659,
+    Stockholm: 2374550,
+    Sörmland: 297169,
+    Uppsala: 383044,
+    Värmland: 282342,
+    Västerbotten: 271621,
+    Västernorrland: 245380,
+    Västmanland: 275634,
+    Västra_Götaland: 1724529,
+    Örebro: 304634,
+    Östergötland: 465214,
+  }
+})
